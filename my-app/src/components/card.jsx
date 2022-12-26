b68b23cf0d9ddaf8e7c3d02090babf477a68e071
RafaelVillagomezDev <rafaelvillagomez.dev@gmail.com>
2022-12-26 16:22:59 +0100

    Fix problem array undefined

--- my-app/src/components/card.jsx
+++ my-app/src/components/card.jsx
@@ -3,43 +3,45 @@ import { useContext } from 'react'
 
 function Card() {
   const data = useContext(PokemonContext)
-  return (
-    <div id="container_card">
-      <div className="card_pokemon">
-        <div className="card_picture">
-          <div className="card_name_pokemon">
-            <div>
-              <h1>{}</h1>
-              <sub>Hierba</sub>
+  return data.results.map((data2) => {
+    return (
+      <div id="container_card">
+        <div className="card_pokemon">
+          <div className="card_picture">
+            <div className="card_name_pokemon">
+              <div>
+                <h1>{}</h1>
+                <sub>Hierba</sub>
 
-              <ul>
-                <li>
-                  Peso<span> 82kg </span>
-                </li>
-                <li>
-                  Altura <span> 2M </span>
-                </li>
-                <li>
-                  Habilidades <span> Cañon</span>
-                </li>
-              </ul>
+                <ul>
+                  <li>
+                    Peso<span> 82kg </span>
+                  </li>
+                  <li>
+                    Altura <span> 2M </span>
+                  </li>
+                  <li>
+                    Habilidades <span> Cañon</span>
+                  </li>
+                </ul>
+              </div>
+              <div>
+                <h1>imagen</h1>
+              </div>
             </div>
-            <div>
-              <h1>imagen</h1>
-            </div>
-          </div>
-          <div className="card_stats_pokemon">
-            <div className="stats_label">
-              <h1>Stats</h1>
-            </div>
-            <div className="stats_pokemon">
-              <h1>Power</h1>
+            <div className="card_stats_pokemon">
+              <div className="stats_label">
+                <h1>Stats</h1>
+              </div>
+              <div className="stats_pokemon">
+                <h1>Power</h1>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  })
 }
 
 export default Card
